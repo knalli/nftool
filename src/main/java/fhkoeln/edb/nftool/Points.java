2cb8732cc4c6825988f0a4538725955ba2170b6b
sja <sebastian@janzen.it>
2011-05-20 10:06:59 +0200

    Removed RooEquals plugin.

--- src/main/java/fhkoeln/edb/nftool/Points.java
+++ src/main/java/fhkoeln/edb/nftool/Points.java
@@ -13,8 +13,6 @@ import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import org.springframework.format.annotation.DateTimeFormat;
 
-import de.saxsys.roo.equals.addon.RooEquals;
-
 import javax.validation.constraints.Min;
 import javax.validation.constraints.Size;
 
@@ -23,8 +21,44 @@ import javax.validation.constraints.Size;
 @RooEntity
 @RooSerializable
 @SuppressWarnings("serial")
-@RooEquals
 public class Points implements Serializable {
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((appKey == null) ? 0 : appKey.hashCode());
+		result = prime * result + ((points == null) ? 0 : points.hashCode());
+		result = prime * result + ((username == null) ? 0 : username.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Points other = (Points) obj;
+		if (appKey == null) {
+			if (other.appKey != null)
+				return false;
+		} else if (!appKey.equals(other.appKey))
+			return false;
+		if (points == null) {
+			if (other.points != null)
+				return false;
+		} else if (!points.equals(other.points))
+			return false;
+		if (username == null) {
+			if (other.username != null)
+				return false;
+		} else if (!username.equals(other.username))
+			return false;
+		return true;
+	}
 
 	@NotNull
 	private String username;
